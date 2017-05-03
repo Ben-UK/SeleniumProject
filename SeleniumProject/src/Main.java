03311487564ba8ecf17e7599a8f0c83d36b74e53
Ben-UK <Ben-UK>
2017-05-03 11:18:31 +0000

    Selenium exersize
    trying to log into facebook with an inncorrect username and password

--- SeleniumProject/src/Main.java
+++ SeleniumProject/src/Main.java
@@ -1,7 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
 
@@ -15,31 +18,43 @@ public class Main {
 
 		driver = new ChromeDriver();
 
-		driver.get("http://www.google.com/");
-		boolean result = true;
+		driver.get("http://www.facebook.com/");
+		String email = "user";
+		String pass = "password";
+
+		WebElement emailBox = driver.findElement(By.name("email"));
+		WebElement passBox = driver.findElement(By.name("pass"));
+		WebElement loginbutton = driver.findElement(By.id("loginbutton"));
+
+		emailBox.sendKeys(email);
+		passBox.sendKeys(pass);
+		loginbutton.click();
+
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		boolean testElement = false;
 
 		try {
-			driver.findElement(By.name("q12fjfhjgfhjghj34"));
+			System.out.println("Searching...");
+			testElement = wait.until(ExpectedConditions.textToBePresentInElementLocated(
+					By.xpath("//*[@id=\"globalContainer\"]/div[3]/div/div/div"),
+					"The email address or phone number that you've entered doesn't match any account. Sign up for an account."));
 
 		}
 
-		catch (NoSuchElementException e) {
+		catch (Exception e) {
 
-			System.out.println(e);
-			result = false;
+			e.printStackTrace();
+			testElement = false;
 
 		}
 
-		finally {
-			driver.close();
-		}
-
-		if (result)
-			System.out.println("---passed---");
+		if (testElement)
+			System.out.println("---Facebook sucesfully stopped the login due to incorrect password---");
 
 		else
-			System.out.println("---failed---");
-
+			System.out.println("---Facebook let you login with just \"admin\" and \"password\"---");
+		driver.close();
+		System.exit(0);
 	}
 
 }
